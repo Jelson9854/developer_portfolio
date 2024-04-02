9ce0293cede894cdeaa72e93a157bdd57094920e
Jelson9854 <Jelson9854@vt.edu>
2024-04-01 22:34:13 -0400

    the day set-up. Will work on at home

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
+import NavBar from '@/component/NavBar';
 import '@/styles/globals.css'
+import {Cormorant_Garamond} from 'next/font/google'
+import Head from 'next/head';
+
+const corm = Cormorant_Garamond({
+  subsets: ['cyrillic'],
+  variable: "--font-corm",
+  weight: '400',
+  display: 'swap',
+});
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className={`${corm.variable} font-corm bg-light w-full min-h-screen`}>
+      <NavBar />
+      <Component {...pageProps} />
+    </main>
+  </>
+  );
 }
+
+
